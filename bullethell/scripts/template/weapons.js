9506704f8f1ac2fd5803dd9bf3d6d2d4c82a51a9
SourDouh <120990895+SourDouh@users.noreply.github.com>
2023-01-04 12:29:38 -0800

    made weapons op
    they fire like 20x more bullets

--- bullethell/scripts/template/weapons.js
+++ bullethell/scripts/template/weapons.js
@@ -2,13 +2,20 @@ const WEAPON = {};
 
 WEAPON.basic = function(pl) {
     emitBullets(pl.pos.x, pl.pos.y, -90, [0], 5, 5, BULLET.small, true);
+    
+    emitBullets(pl.pos.x - 20, pl.pos.y, -90, [-20,-10,0,10,20], 5, 5, BULLET.small, true);
+    emitBullets(pl.pos.x + 20, pl.pos.y, -90, [-20,-10,0,10,20], 5, 5, BULLET.small, true);
 };
 
 WEAPON.dualFire = function(pl) {
-    emitBullets(pl.pos.x - 5, pl.pos.y, -90, [0], 5, 5, BULLET.small, true);
-    emitBullets(pl.pos.x + 5, pl.pos.y, -90, [0], 5, 5, BULLET.small, true);
+    emitBullets(pl.pos.x - 20, pl.pos.y, -90, [-20,-10,0,10,20], 5, 5, BULLET.small, true);
+    emitBullets(pl.pos.x + 20, pl.pos.y, -90, [-20,-10,0,10,20], 5, 5, BULLET.small, true);
 };
 
 WEAPON.tripleFire = function(pl) {
     emitBullets(pl.pos.x, pl.pos.y, -90, [-8, 0, 8], 5, 5, BULLET.small, true);
+};
+
+WEAPON.bigBomb = function(pl) {
+    emitBullets(pl.pos.x, pl.pos.y, -90, [random(-10,10)], 5, 5, BULLET.large, true);
 };
